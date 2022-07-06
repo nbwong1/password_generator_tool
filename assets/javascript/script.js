6c720c66cec2ebbc2cfc6db36e01d515f0fa0060
nbwong <nicholas.b.wong@gmail.com>
2022-07-05 23:14:07 -0700

    updated with more criteria and prompts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,18 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // create generatePassword function//
 function generatePassword(event) {
-  console.log("This button works!");
-  enter = prompt("How many characters would you like your password to be?")
-  if (enter == null){
-    alert(length is required!){
+  enter = prompt("How many characters would you like your password to be?");
+  if (!enter) {
+    alert("length is required!");
   } else if (enter < 8 || enter > 128) {
-    enter = prompt("You must choose between 8 and 128 characters!")
-  } else 
+    enter = prompt("You must choose between 8 and 128 characters!");
+  } else {
     confirmNumber = confirm("Will this password have numbers?");
     confirmSpecialCharacters = confirm("Will this password have special characters?");
     confirmUpperCase = confirm("Will this password have Upper Case letters?");
     confirmLowerCase = confirm("Will this password have Lower Case letters?");
-    }
+  }
 }
 
 // Write password to the #password input
