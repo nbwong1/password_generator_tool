87c9556fa631a8cdf74f0f0a29bda6aae72e001d
nbwong <nicholas.b.wong@gmail.com>
2022-07-05 23:26:30 -0700

    I have updated my code even more to include comments, will complete the rest of the assignment over the weekend.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var options;
+var numbers = '0123456789'
+var specialCharacters = "!#$%&'(*+,-./\:;<=>?@[]^_`{|}~"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 
 // create generatePassword function//
 function generatePassword(event) {
   enter = prompt("How many characters would you like your password to be?");
+//check if any length is entered//
   if (!enter) {
     alert("length is required!");
+//set min and max length for password//
   } else if (enter < 8 || enter > 128) {
     enter = prompt("You must choose between 8 and 128 characters!");
+//criteria must be met for password, choose: numbers, special characters, upper case, and/or lower case//
   } else {
     confirmNumber = confirm("Will this password have numbers?");
     confirmSpecialCharacters = confirm("Will this password have special characters?");
     confirmUpperCase = confirm("Will this password have Upper Case letters?");
     confirmLowerCase = confirm("Will this password have Lower Case letters?");
   }
+//confirm criteria has been selected//
+  if (!confirmNumber && !confirmSpecialCharacters && !confirmUpperCase && !confirmLowerCase) {
+    options = alert("You need to select criteria.")
+  }
+//
 }
 
 // Write password to the #password input
