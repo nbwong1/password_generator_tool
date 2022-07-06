fc3522da5745f506e5be81b00db3e6a63a6f6ba6
nbwong <nicholas.b.wong@gmail.com>
2022-07-05 18:23:19 -0700

    Starting to add my code to the file, starting with prompts

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword) {
+  let writePassword = prompt('How long should the password be?');
+}
+//Generate Button Prompts//
